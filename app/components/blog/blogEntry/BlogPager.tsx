24f7b2791ca769ac869c894eeac91ba38d2f5318
sside <sside@live.jp>
2022-08-12 00:39:28 +0900

    feat(blog): モバイル向けビューポート対応

--- app/components/blog/blogEntry/BlogPager.tsx
+++ app/components/blog/blogEntry/BlogPager.tsx
@@ -1,6 +1,6 @@
 import { LinksFunction } from "@remix-run/node";
 import { Link } from "@remix-run/react";
-import { FC } from "react";
+import { FC, Fragment } from "react";
 import { cssLinks } from "../../../utilities/styling/cssLinkDescriptor";
 import styles from "./BlogPager.css";
 
@@ -18,17 +18,21 @@ interface Props {
 
 export const BlogPager: FC<Props> = ({ previous, next }) => {
     return (
-        <nav className={`blogPager`}>
-            {previous && (
-                <Link to={previous.url} className={`blogPager__link--previous`}>
-                    {previous.label}
-                </Link>
+        <Fragment>
+            {(previous || next) && (
+                <nav className={`blogPager`}>
+                    {previous && (
+                        <Link to={previous.url} className={`blogPager__link--previous`}>
+                            {previous.label}
+                        </Link>
+                    )}
+                    {next && (
+                        <Link to={next.url} className={`blogPager__link--next`}>
+                            {next.label}
+                        </Link>
+                    )}
+                </nav>
             )}
-            {next && (
-                <Link to={next.url} className={`blogPager__link--next`}>
-                    {next.label}
-                </Link>
-            )}
-        </nav>
+        </Fragment>
     );
 };
